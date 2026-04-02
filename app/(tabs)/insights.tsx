4bb95a999d92d05a561e75db761e1eb6c442e9d0
jaminjamts <jamka888@gmail.com>
2026-04-02 20:47:01 +0800

    feat: project setup, nativewind, routing, tab navigation

---
+++ app/(tabs)/insights.tsx
@@ -0,0 +1,15 @@
+import { styled } from "nativewind";
+import React from "react";
+import { Text, View } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView);
+
+const Insights = () => {
+  return (
+    <View>
+      <Text>insights</Text>
+    </View>
+  );
+};
+
+export default Insights;
